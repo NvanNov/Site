77a073af5647de3cf7b31f2706fa78015dc5209c
NvanNov <loginnovickoff@yandex.ru>
2023-04-20 18:45:55 +0300

    Initial commit

---
+++ flask_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    params = {}
+    params["title"] = title
+    return render_template('index.html', **params)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    params = {}
+    params["title"] = prof
+    params["profession"] = prof.lower()
+    return render_template('training.html', **params)
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
